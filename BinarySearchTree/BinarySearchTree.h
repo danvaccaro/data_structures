eba68fa69568729097276b537bb750a39e0ae8ea
Dan Vaccaro <dv.dev11@pm.me>
2023-04-28 12:32:52 -0400

    added interface declarations

---
+++ BinarySearchTree/BinarySearchTree.h
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using KeyType = int;
+using ValueType = std::string;
+
+struct BSTNode
+{
+    KeyType key;
+    ValueType val;
+    BSTNode *left, *right;
+};
+
+class BinarySearchTree
+{
+public:
+    bool isEmpty();
+    int size();
+    bool contains(KeyType searchKey);
+    void insert(KeyType key, ValueType value);
+    void remove(KeyType key);
+    ValueType lookup(KeyType key);
+    KeyType min();
+    KeyType max();
+
+private:
+    BSTNode *root;
+    int size;
+    BSTNode *newNode(KeyType key, ValueType value, BSTNode *left, BSTNode *right);
+    BSTNode *newNode(KeyType key, ValueType value);
+    bool isEmpty(BSTNode *tree);
+    bool contains(KeyType key, BSTNode *tree);
+    KeyType min(BSTNode *tree);
+    bool isLeaf(BSTNode *tree);
+};
